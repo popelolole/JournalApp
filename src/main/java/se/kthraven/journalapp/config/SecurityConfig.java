d47e272dcd6e75fe0c0579fb5b7b200318454e16
popelolole <pellebe@kth.se>
2023-11-15 01:30:22 +0100

    config: disable csrf enable httpbasic

--- src/main/java/se/kthraven/journalapp/config/SecurityConfig.java
+++ src/main/java/se/kthraven/journalapp/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package se.kthraven.journalapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,7 +34,8 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
-                .formLogin(withDefaults())
+                .httpBasic(basic -> basic.init(http))
+                .csrf(csrf -> csrf.disable())
                 .build();
     }
 
